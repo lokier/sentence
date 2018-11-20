9b413bdb8af3c988a4a19d7d3aa7958b5bb9cbe2
lokier <raodongming@hotmail.com>
2018-11-20 08:28:57 +0800

    init

--- Jfinal/src/demo/HelloController.java
+++
@@ -1,7 +0,0 @@
-package demo;
-import com.jfinal.core.Controller;
-public class HelloController extends Controller {
-    public void index() {
-       renderText("Hello JFinal World.Mr Rao");
-    }
-}
